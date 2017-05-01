4d4361030740d53de46c2e92268fa4ebc56f8ee0
Kelvin Luck <git@kelvinluck.com>
2017-05-01 13:31:00 +0100

    Make `ember-cli-typescript` a dependency (not devDependency)
    After much chasing around the houses it seems like this is all that is
    necessary to make typescript work correctly in an addon!

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 /* eslint-env node */
 'use strict';
 
-const TsPreprocessor = require('ember-cli-typescript/lib/typescript-preprocessor');
-
 module.exports = {
   name: 'addon-with-typescript',
-
-  setupPreprocessorRegistry: function(type, registry) {
-    try {
-      var plugin = new TsPreprocessor({includeExtensions: ['.ts','.js']});
-      registry.add('js', plugin);
-    } catch ( ex ) {
-      console.log( "Missing or invalid tsconfig.json, please fix or run `ember generate ember-cli-typescript`." );
-      console.log( '  ' + ex.toString());
-    }
-  }
-
 };
